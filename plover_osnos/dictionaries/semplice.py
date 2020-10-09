14e72e1ec2d1a01b96d45bd22843e26173551d36
Noelia Ruiz Martínez <nrm1977@gmail.com>
2020-10-10 00:13:22 +0200

    Add numbers, semplice and verification for completa

--- plover_osnos/dictionaries/semplice.py
+++ plover_osnos/dictionaries/semplice.py
@@ -1,8 +1,22 @@
 LONGEST_KEY = 1
 
+numbers = {
+	"S": "1",
+	"P": "2",
+	"T": "3",
+	"V": "4",
+	"I": "5",
+	"O": "0",
+	"c": "6",
+	"t": "7",
+	"p": "8",
+	"i": "9"
+}
+
+
 dict = {
-	"SC": ("casa", "ca"),
-	"N": ("nana", "n")
+	"SC": ("casa ", "ca", True),
+	"N": ("nana ", "n", False)
 }
 
 lastValue = ""
@@ -10,12 +24,27 @@ lastValue = ""
 def lookup(key):
 	global lastValue
 	value = ""
-	if dict.get(key[0]) is not None:
-		value = dict.get(key[0])[0] 
+	# Numbers
+	if "#" in key[0]:
+		for k in key[0]:
+			if k in ("A", "#"):
+				continue
+			numberValue = numbers.get(k)
+			if numberValue is None:
+				break
+			value += numberValue
+		if value != "" and "A" in key[0]:
+			if len(value) > 1:
+				value = value[::-1]
+			else:
+				value = "{firstDigit}{secondDigit}".format(firstDigit=value, secondDigit=value)
+	if value == "" and dict.get(key[0]) is not None:
+		if lastValue == "" or lastValue.endswith(" ") or not dict.get(key[0])[2]:
+			value = dict.get(key[0])[0] 
 		if value == "":
 			value = dict.get(key[0])[1]
 	if value == "":
-		searchKey = key[0][:]
+		searchKey = key[0][:-1]
 		searchKeyValue = ""
 		while len(searchKey) > 0:
 			if dict.get(searchKey) is not None:
@@ -27,4 +56,6 @@ def lookup(key):
 				searchKeyValue = ""
 				searchKey = key[0][len(searchKey):]
 	lastValue = value
-	return value
+	if value.endswith(" "):
+		return value
+	return "{^}" + value
